46b771733a918d081ccd9a84ea8541d6a0d7dfc3
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-04-28 22:57:00 +0200

    Improved backend

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,16 +4,16 @@ const bodyParser = require('body-parser');
 module.exports = (app) => {
 
     app.use(bodyParser());
-    
-    app.get('/', (req, res) => res.send('Hello World!'))
+
+    app.get('/', (req, res) => res.send('This is the coaich backend!'))
 
     app.post('/tone', (req, res) => {
-        if (!req || !(req.body) || !(req.body.text)) {
+        if (!req || !req.body || !req.body.text) {
             return res.send('No text received. Please use the following JSON format: {"text" : "this is example text"}');
         }
         new Tone().analyze(req.body.text).then(tone => {
             return res.send(tone);
         });
     });
-
+    
 }
