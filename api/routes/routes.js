f62a2b7b025d17815f5620064ba7e5a255185d1f
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-04-28 22:54:12 +0200

    Error feedback in api

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 module.exports = (app) => {
 
     app.use(bodyParser());
+    
     app.get('/', (req, res) => res.send('Hello World!'))
 
     app.post('/tone', (req, res) => {
-        console.log('tone request', req.body);
+        if (!req || !(req.body) || !(req.body.text)) {
+            return res.send('No text received. Please use the following JSON format: {"text" : "this is example text"}');
+        }
         new Tone().analyze(req.body.text).then(tone => {
-            res.send(tone)
+            return res.send(tone);
         });
     });
 
