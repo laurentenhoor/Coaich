8d64eb001baf199ff4e007fc788df0afb6316183
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-05-11 21:19:35 +0000

    Added CORS support on backend

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -11,6 +11,14 @@ module.exports = (app) => {
         if (!req || !req.body || !req.body.text) {
             return res.send('No text received. Please use the following JSON format: {"text" : "this is example text"}');
         }
+
+// CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    // Set custom headers for CORS
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Content-type,Accept,X-Custom-Header");
+
+
         new Tone().analyze(req.body.text).then(tone => {
             return res.send(tone);
         });
