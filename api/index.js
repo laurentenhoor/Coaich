fb58481865d1148ce2487531b4d0968c6c1f6181
Lauren ten Hoor <laurentenhoor@gmail.com>
2018-05-12 09:04:11 +0200

    Refactoring CORS headers

--- api/index.js
+++ api/index.js
@@ -3,18 +3,4 @@ const app = express();
 
 require('./routes/routes')(app);
 
-app.use(function (req, res, next) {
-    // CORS headers
-    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    // Set custom headers for CORS
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Content-type,Accept,X-Custom-Header");
-
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
-
-    return next();
-});
-
 app.listen(3000, () => console.log('Coaich backend listening on port 3000!'))
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,23 +5,33 @@ module.exports = (app) => {
 
     app.use(bodyParser());
 
+    app.use((req, res, next) => {
+        addCorsHeaders(res);
+        if (req.method === "OPTIONS") {
+            return res.status(200).end();
+        }
+        return next();
+    });
+
     app.get('/', (req, res) => res.send('This is the coaich backend!'))
 
     app.post('/tone', (req, res) => {
         if (!req || !req.body || !req.body.text) {
             return res.send('No text received. Please use the following JSON format: {"text" : "this is example text"}');
         }
-
-// CORS headers
-    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    // Set custom headers for CORS
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Content-type,Accept,X-Custom-Header");
-
+        addCorsHeaders(res);
 
         new Tone().analyze(req.body.text).then(tone => {
             return res.send(tone);
         });
     });
-    
+
+}
+
+function addCorsHeaders(res) {
+     // CORS headers
+     res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+     // Set custom headers for CORS
+     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Content-type,Accept");
 }
